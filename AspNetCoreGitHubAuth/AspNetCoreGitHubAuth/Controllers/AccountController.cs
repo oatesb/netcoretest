ffc2e8901491d5160db6e5436839b46b59bf1c3e
Ben Oates <oatesb@gmail.com>
2018-12-01 11:56:15 -0800

    git auth testing

---
+++ AspNetCoreGitHubAuth/AspNetCoreGitHubAuth/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreGitHubAuth.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
